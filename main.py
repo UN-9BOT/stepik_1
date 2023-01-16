9db12ebfd77abceca172e73e62ae375d5fb33e93
un-9bot <un.9bot@gmail.com>
2023-01-16 17:15:14 +0300

    next 4.1

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-word = [i for i in input()]
-a = word[0]
-b = word[1]
-c = word[2]
-d = word[3]
-print(f'Цифра в позиции тысяч равна {a}')
-print(f'Цифра в позиции сотен равна {b}')
-print(f'Цифра в позиции десятков равна {c}')
-print(f'Цифра в позиции единиц равна {d}')
+n = input()
+w = input()
+if n == w:
+    print('Пароль принят')
+else:
+    print('Пароль не принят')
